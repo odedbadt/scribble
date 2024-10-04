7dbfbaab5bf464e599f56b23cd81d275962354df
Oded Badt <odedbadt@gmail.com>
2024-10-04 15:09:18 +0300

    Fixed dropper

--- static/js/dropper.js
+++ static/js/dropper.js
@@ -7,7 +7,7 @@ export class Dropper {
         }
     start(from) {
         const color = this.app.art_context.getImageData(from[0],from[1],1,1).data;
-        const pen_color = `rgb(${color[0]},${color[1]},${color[2]})`;        
+        const pen_color = `rgba(${color[0]},${color[1]},${color[2]},255)`;        
         this.app.settings.fore_color = pen_color;
         if (this.applier.previous_tool_name) {
             this.app.select_tool(this.applier.previous_tool_name)
