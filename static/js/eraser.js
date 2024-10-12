850ec647797eb031e9b040c32a870bea76b3d8c9
Oded Badt <odedbadt@gmail.com>
2024-10-12 12:47:20 +0300

    A bit of adjustments to eraser colors

--- static/js/eraser.js
+++ static/js/eraser.js
@@ -5,13 +5,17 @@ export class EraserTool extends ClickAndDragTool{
         super(context, applier, true, tmp_context);
         }
     hover_action(at) {
-        this.tmp_context.fillStyle = 'rgb(128,128,128,255)'
+        this.tmp_context.fillStyle = this.app.settings.back_color
+        this.tmp_context.strokeStyle = this.app.settings.fore_color
+        
         this.tmp_context.lineWidth = 1
         this.tmp_context.beginPath();
         const r = 25;//this.app.settings.line_width / 2;
         this.tmp_context.ellipse(
             at[0],at[1],r,r,0,0,Math.PI*2);
+        this.tmp_context.fill()
         this.tmp_context.stroke()
+        
     
     }
     
