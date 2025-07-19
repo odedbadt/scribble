2eb8869496193a17aef2a087e5867bdcff128d92
Oded Badt <odedbadt@gmail.com>
2025-07-19 16:14:48 +0300

    Clear all works

--- src/ts/clearall.ts
+++ src/ts/clearall.ts
@@ -1,8 +1,9 @@
 import { EditingTool } from "./editing_tool";
 import { Editor } from "./editor";
 import { SettingName, settings } from "./settings_registry";
-import { Vector2 } from "./types";
+import { RectToRectMapping, Vector2 } from "./types";
 import { extend_canvas_mapping, tool_to_document } from "./utils";
+import { batch } from "@preact/signals";
 
 export class ClearAllTool extends EditingTool {
 
@@ -19,7 +20,16 @@ export class ClearAllTool extends EditingTool {
         this.context!.fillStyle = color;
         this.context!.fillRect(0, 0, w, h);
         this.stop({ x: 0, y: 0 });
-
+        const rect_mapping: RectToRectMapping = {
+            from: { x: 0, y: 0, w: 1, h: 1 },
+            to: { x: 0, y: 0, w: dw, h: dh },
+        }
+        tool_to_document(this.canvas!,
+            rect_mapping, this.document_context!);
+        batch(() => {
+            this.canvas_bounds_mapping_signal!.value = rect_mapping;
+            this.canvas_signal!.value = this.canvas!;
+        })
     }
     start(at: Vector2, buttons: number): boolean {
         throw new Error("Method not implemented.");
@@ -28,14 +38,6 @@ export class ClearAllTool extends EditingTool {
         throw new Error("Method not implemented.");
     }
     stop(at: Vector2) {
-        if (this.document_context == null) {
-            throw new Error("Cannot stop tool if editor is not full initialized")
-        }
-        tool_to_document(this.canvas!,
-            this.canvas_bounds_mapping_signal!.value, this.document_context);
-        console.log('A');
-        this.canvas_bounds_mapping_signal!.value = this.canvas_bounds_mapping_signal!.value;
-        console.log('B');
     }
     select_action() {
 
