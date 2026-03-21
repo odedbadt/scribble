66935f72ecb9571db9c93f6ce6468a7046d1edbf
Oded Badt <odedbadt@gmail.com>
2026-03-21 16:52:12 +0200

    Fixed eraser

--- src/ts/eraser.ts
+++ src/ts/eraser.ts
@@ -3,16 +3,17 @@ import { SettingName, settings } from "./settings_registry";
 import { parseColor, RGBA } from "./pixel_utils";
 
 export class EraserTool extends ScribbleTool {
-    // Eraser uses white (or could use background color)
-    private _eraser_color: RGBA = [255, 255, 255, 255];
 
     constructor() {
         super();
-        this._stroke_color = this._eraser_color;
     }
 
     editing_start() {
         const colorStr = settings.peek<string>(SettingName.BackColor);
         this._stroke_color = parseColor(colorStr);
+    }
+
+    commit_to_document(_color: string | null = null) {
+        super.commit_to_document(settings.peek<string>(SettingName.BackColor));
     }
 }
