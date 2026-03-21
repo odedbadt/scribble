b20df5874145c35f7e7492151c6e8cbdf4b86a30
Oded Badt <odedbadt@gmail.com>
2026-03-21 15:39:05 +0200

    Fixed cleal all

--- src/ts/clearall.ts
+++ src/ts/clearall.ts
@@ -1,47 +1,20 @@
 import { EditingTool } from "./editing_tool";
-import { Editor } from "./editor";
 import { SettingName, settings } from "./settings_registry";
-import { RectToRectMapping, Vector2 } from "./types";
-import { tool_to_document } from "./utils";
+import { Vector2 } from "./types";
 
 export class ClearAllTool extends EditingTool {
 
     select() {
-        if (this.document_canvas == null) {
-            throw new Error("Cannot select tool if editor is not full initialized")
+        if (this.document_canvas == null || this.document_context == null) {
+            throw new Error("Cannot select tool if editor is not fully initialized")
         }
-        const dw = this.document_canvas.clientWidth;
-        const dh = this.document_canvas.clientHeight;
-        this.canvas_bounds_mapping = {
-            from: { x: 0, y: 0, w: 1, h: 1 },
-            to: { x: 0, y: 0, w: dw, h: dh }
-        }
-        const w = this.canvas!.clientWidth;
-        const h = this.canvas!.clientHeight;
         const color = settings.peek<string>(SettingName.BackColor);
-        this.context!.fillStyle = color;
-        this.context!.fillRect(0, 0, w, h);
-        this.stop({ x: 0, y: 0 });
-        const rect_mapping: RectToRectMapping = {
-            from: { x: 0, y: 0, w: 1, h: 1 },
-            to: { x: 0, y: 0, w: dw, h: dh },
-        }
-        tool_to_document(this.canvas!,
-            rect_mapping, this.document_context!);
-        this.publish_signals();
+        this.document_context.fillStyle = color;
+        this.document_context.fillRect(0, 0, this.document_canvas.width, this.document_canvas.height);
+        this.document_dirty_signal!.value++;
     }
-    start(at: Vector2, buttons: number): boolean {
-        throw new Error("Method not implemented.");
-    }
-    drag(at: Vector2): boolean {
-        throw new Error("Method not implemented.");
-    }
-    stop(at: Vector2) {
-    }
-    select_action() {
-
-    }
-    hover(): boolean {
-        return false
-    }
+    start(at: Vector2, buttons: number) {}
+    drag(at: Vector2) {}
+    stop(at: Vector2) {}
+    hover(at: Vector2) {}
 }
