abde3cb85976edd3291aa23e97bb0db2efc1899a
Oded Badt <odedbadt@gmail.com>
2026-03-23 11:07:09 +0200

    dropper works

--- src/ts/dropper.ts
+++ src/ts/dropper.ts
@@ -1,34 +1,36 @@
-import { ClickTool } from "./click_tool"
 import { EditingTool } from "./editing_tool";
-import { Editor } from "./editor";
 import { SettingName, settings } from "./settings_registry";
 import { state_registry, StateValue } from "./state_registry";
 import { Vector2 } from "./types";
+
 export class Dropper extends EditingTool {
+    private _buttons: number = 0;
+
+    private _pick(at: Vector2) {
+        if (this.document_context == null) return;
+        const data = this.document_context.getImageData(at.x, at.y, 1, 1).data;
+        const color_string = `rgba(${data[0]},${data[1]},${data[2]},255)`;
+        const is_fore = !!(this._buttons & 1);
+        settings.set(is_fore ? SettingName.ForeColor : SettingName.BackColor, color_string);
+    }
 
     start(at: Vector2, buttons: number) {
-        if (this.document_context == null) {
-            throw new Error("Cannot edit if editor is not full initialized")
-        }
-        const color = this.document_context.getImageData(at.x, at.y, 1, 1).data;
-        const is_fore = !!(buttons & 1);
-        settings.set(is_fore ? SettingName.ForeColor : SettingName.BackColor,
-            color);
+        this._buttons = buttons;
+        this._pick(at);
+    }
+
+    drag(at: Vector2) {
+        this._pick(at);
     }
 
     hover(at: Vector2): boolean {
-        return false
-    }
-    select() {
-
-    }
-    drag(at: Vector2) {
-
-    }
-    stop(at: Vector2): boolean {
-        super.stop(at);
-        this.canvas_signal!.value = null;
-        state_registry.pop(StateValue.SelectedToolName)
         return false;
+    }
+
+    select() {}
+
+    stop(at: Vector2) {
+        super.stop(at);
+        state_registry.pop(StateValue.SelectedToolName);
     }
 }
